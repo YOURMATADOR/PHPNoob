c8ea10dc838f9fd7777fc96701c400691fa6da97
YOURMATADOR <jesus.amancilla@alumnos.udg.mx>
2018-08-29 19:03:52 -0500

    estructuras de control :sunglasses:

---
+++ public/cap2/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Arrays</title>
+</head>
+<body>
+    <h1>arreglos</h1>
+    <?php
+    $arreglo = [1, 2, 3, 4];
+    $arregloDos = array(2, 3, 4, 5, 6);
+    print_r($arreglo);
+    print('Hola');
+    print_r($arregloDos);
+    $arregloDos['nombre'] = 10;
+    echo $arregloDos['nombre'] . 'mi nombre';
+
+    $miObjeto->nombre = "Eduardo";
+    $miObjeto->edad = 20;
+
+    echo json_encode($miObjeto);
+    $miNombre = json_encode($miObjeto);
+    e
+    ?>
+</body>
+</html>
